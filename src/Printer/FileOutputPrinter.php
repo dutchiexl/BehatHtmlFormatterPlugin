6670b0f9fc4fcc2a7b670676b0288ea64246b9af
Neal Vanmeert <neal@emuse.be>
2015-03-03 14:30:50 +0100

    Added asset folder copying to report folder.

--- src/Printer/FileOutputPrinter.php
+++ src/Printer/FileOutputPrinter.php
@@ -6,158 +6,180 @@
 
 namespace emuse\BehatHTMLFormatter\Printer;
 
-use Behat\Testwork\Output\Printer\OutputPrinter as PrinterInterface;
 use Behat\Testwork\Output\Exception\BadOutputPathException;
+use Behat\Testwork\Output\Printer\OutputPrinter as PrinterInterface;
 
 
-class FileOutputPrinter implements PrinterInterface
-{
+class FileOutputPrinter implements PrinterInterface {
 
-    /**
-     * @param  $outputPath where to save the generated report file
-     */
-    private $outputPath;
+  /**
+   * @param  $outputPath where to save the generated report file
+   */
+  private $outputPath;
 
-    /**
-     * @param  $base_path Behat base path
-     */
-    private $base_path;
+  /**
+   * @param  $base_path Behat base path
+   */
+  private $base_path;
 
 
-    public function __construct($base_path)
-    {
-        $this->base_path = $base_path;
+  public function __construct($base_path) {
+    $this->base_path = $base_path;
+  }
+
+  /**
+   * Verify that the specified output path exists or can be created,
+   * then sets the output path.
+   *
+   * @param String $path Output path relative to %paths.base%
+   *
+   */
+  public function setOutputPath($path) {
+    $outpath = $path;
+    if (!file_exists($outpath)) {
+      if (!mkdir($outpath, 0755, TRUE)) {
+        throw new BadOutputPathException(
+          sprintf(
+            'Output path %s does not exist and could not be created!',
+            $outpath
+          ),
+          $outpath
+        );
+      }
     }
+    else {
+      if (!is_dir(realpath($outpath))) {
+        throw new BadOutputPathException(
+          sprintf(
+            'The argument to `output` is expected to the a directory, but got %s!',
+            $outpath
+          ),
+          $outpath
+        );
+      }
+    }
+    $this->outputPath = $outpath;
+  }
 
-    /**
-     * Verify that the specified output path exists or can be created,
-     * then sets the output path.
-     *
-     * @param String $path Output path relative to %paths.base%
-     *
-     */
-    public function setOutputPath($path)
-    {
-        $outpath = $path;
-        if (!file_exists($outpath)) {
-            if (!mkdir($outpath, 0755, true))
-                throw new BadOutputPathException(
-                    sprintf(
-                        'Output path %s does not exist and could not be created!',
-                        $outpath
-                    ),
-                    $outpath
-                );
-        } else {
-            if (!is_dir(realpath($outpath))) {
-                throw new BadOutputPathException(
-                    sprintf(
-                        'The argument to `output` is expected to the a directory, but got %s!',
-                        $outpath
-                    ),
-                    $outpath
-                );
-            }
+  /**
+   * Returns output path
+   *
+   * @return String output path
+   */
+  public function getOutputPath() {
+    return $this->outputPath;
+  }
+
+  /**
+   * Sets output styles.
+   *
+   * @param array $styles
+   */
+  public function setOutputStyles(array $styles) {
+
+  }
+
+  /**
+   * Returns output styles.
+   *
+   * @return array
+   */
+  public function getOutputStyles() {
+
+  }
+
+  /**
+   * Forces output to be decorated.
+   *
+   * @param Boolean $decorated
+   */
+  public function setOutputDecorated($decorated) {
+
+  }
+
+  /**
+   * Returns output decoration status.
+   *
+   * @return null|Boolean
+   */
+  public function isOutputDecorated() {
+    return TRUE;
+  }
+
+  /**
+   * Sets output verbosity level.
+   *
+   * @param integer $level
+   */
+  public function setOutputVerbosity($level) {
+
+  }
+
+  /**
+   * Returns output verbosity level.
+   *
+   * @return integer
+   */
+  public function getOutputVerbosity() {
+
+  }
+
+  /**
+   * Writes message(s) to output console.
+   *
+   * @param string|array $messages message or array of messages
+   */
+  public function write($messages) {
+    $file = $this->outputPath . DIRECTORY_SEPARATOR . 'report.html';
+    file_put_contents($file, $messages);
+    $this->copyAssets();
+  }
+
+  /**
+   * Copies the assets folder to the report destination.
+   */
+  public function copyAssets() {
+    // If the assets folder doesn' exist in the output path, copy it
+    $source = realpath(dirname(__FILE__));
+    $assets_source = realpath($source . "/../../assets");
+    $destination = $this->outputPath . DIRECTORY_SEPARATOR . 'assets';
+    $this->recurse_copy($assets_source, $destination);
+  }
+
+  /**
+   * Recursivly copy a path.
+   * @param $src
+   * @param $dst
+   */
+  private function recurse_copy($src, $dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while (FALSE !== ($file = readdir($dir))) {
+      if (($file != '.') && ($file != '..')) {
+        if (is_dir($src . '/' . $file)) {
+          $this->recurse_copy($src . '/' . $file, $dst . '/' . $file);
         }
-        $this->outputPath = $outpath;
+        else {
+          copy($src . '/' . $file, $dst . '/' . $file);
+        }
+      }
     }
+    closedir($dir);
+  }
 
-    /**
-     * Returns output path
-     *
-     * @return String output path
-     */
-    public function getOutputPath()
-    {
-        return $this->outputPath;
-    }
+  /**
+   * Writes newlined message(s) to output console.
+   *
+   * @param string|array $messages message or array of messages
+   */
+  public function writeln($messages = '') {
 
-    /**
-     * Sets output styles.
-     *
-     * @param array $styles
-     */
-    public function setOutputStyles(array $styles)
-    {
+  }
 
-    }
+  /**
+   * Clear output console, so on next write formatter will need to init (create) it again.
+   */
+  public function flush() {
 
-    /**
-     * Returns output styles.
-     *
-     * @return array
-     */
-    public function getOutputStyles()
-    {
-
-    }
-
-    /**
-     * Forces output to be decorated.
-     *
-     * @param Boolean $decorated
-     */
-    public function setOutputDecorated($decorated)
-    {
-
-    }
-
-    /**
-     * Returns output decoration status.
-     *
-     * @return null|Boolean
-     */
-    public function isOutputDecorated()
-    {
-        return true;
-    }
-
-    /**
-     * Sets output verbosity level.
-     *
-     * @param integer $level
-     */
-    public function setOutputVerbosity($level)
-    {
-
-    }
-
-    /**
-     * Returns output verbosity level.
-     *
-     * @return integer
-     */
-    public function getOutputVerbosity()
-    {
-
-    }
-
-    /**
-     * Writes message(s) to output console.
-     *
-     * @param string|array $messages message or array of messages
-     */
-    public function write($messages)
-    {
-        $file = $this->outputPath . DIRECTORY_SEPARATOR . 'report.html';
-        file_put_contents($file, $messages);
-    }
-
-    /**
-     * Writes newlined message(s) to output console.
-     *
-     * @param string|array $messages message or array of messages
-     */
-    public function writeln($messages = '')
-    {
-
-    }
-
-    /**
-     * Clear output console, so on next write formatter will need to init (create) it again.
-     */
-    public function flush()
-    {
-
-    }
+  }
 }
