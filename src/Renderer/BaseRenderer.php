c658c9b8fb2b8284e60b31ba745ccd0a56ed2af5
DaSayan <glennwall@free.fr>
2015-03-12 18:42:51 +0100

    WIP abstraction layer for rendering

--- src/Renderer/BaseRenderer.php
+++ src/Renderer/BaseRenderer.php
@@ -51,4 +51,114 @@ class BaseRenderer
         $print = $this->rendererTab[0]->renderAfterExercise($obj) ;
         return $print ;
     }    
+    
+    /**
+     * Renders before a suite.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */        
+    public function renderBeforeSuite($obj) {
+        $print = $this->rendererTab[0]->renderBeforeSuite($obj) ;
+        return $print ;
+    }     
+
+    /**
+     * Renders after a suite.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */     
+    public function renderAfterSuite($obj) {
+        $print = $this->rendererTab[0]->renderAfterSuite($obj) ;
+        return $print ;
+    } 
+    
+    /**
+     * Renders before a feature.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */        
+    public function renderBeforeFeature($obj) {
+        $print = $this->rendererTab[0]->renderBeforeFeature($obj) ;
+        return $print ;
+    }     
+
+    /**
+     * Renders after a feature.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */     
+    public function renderAfterFeature($obj) {
+        $print = $this->rendererTab[0]->renderAfterFeature($obj) ;
+        return $print ;
+    }    
+
+    /**
+     * Renders before a scenario.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */            
+    public function renderBeforeScenario($obj) {
+        $print = $this->rendererTab[0]->renderBeforeScenario($obj) ;
+        return $print ;
+    }     
+
+    /**
+     * Renders after a scenario.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */     
+    public function renderAfterScenario($obj) {
+        $print = $this->rendererTab[0]->renderAfterScenario($obj) ;
+        return $print ;
+    }   
+    
+    /**
+     * Renders before an outline.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */            
+    public function renderBeforeOutline($obj) {
+        $print = $this->rendererTab[0]->renderBeforeOutline($obj) ;
+        return $print ;
+    }
+    
+    /**
+     * Renders after an outline.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */     
+    public function renderAfterOutline($obj) {
+        $print = $this->rendererTab[0]->renderAfterOutline($obj) ;
+        return $print ;
+    } 
+    
+    /**
+     * Renders before a step.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */        
+    public function renderBeforeStep($obj) {
+        $print = $this->rendererTab[0]->renderBeforeStep($obj) ;
+        return $print ;
+    }
+    
+    /**
+     * Renders after a step.
+     *
+     * @param object   : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */        
+    public function renderAfterStep($obj) {
+        $print = $this->rendererTab[0]->renderAfterStep($obj) ;
+        return $print ;
+    }   
 }
