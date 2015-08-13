e55bcd69e1a33e6e24314e5a0db555d77b88012d
Neal Vanmeert <neal@emuse.be>
2015-08-13 10:02:23 +0200

    Added Interface

---
+++ src/Renderer/RendererInterface.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nealv
+ * Date: 12/08/15
+ * Time: 10:45
+ */
+
+namespace emuse\BehatHTMLFormatter\Renderer;
+
+interface RendererInterface {
+
+    /**
+     * Renders before an exercice.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderBeforeExercise($obj);
+
+    /**
+     * Renders after an exercice.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderAfterExercise($obj);
+
+    /**
+     * Renders before a suite.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderBeforeSuite($obj);
+
+    /**
+     * Renders after a suite.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderAfterSuite($obj);
+
+    /**
+     * Renders before a feature.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderBeforeFeature($obj);
+
+    /**
+     * Renders after a feature.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderAfterFeature($obj);
+
+    /**
+     * Renders before a scenario.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderBeforeScenario($obj);
+
+    /**
+     * Renders after a scenario.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderAfterScenario($obj);
+
+    /**
+     * Renders before an outline.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderBeforeOutline($obj);
+
+    /**
+     * Renders after an outline.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderAfterOutline($obj);
+
+    /**
+     * Renders before a step.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderBeforeStep($obj);
+
+    /**
+     * Renders after a step.
+     * @param object : BehatHTMLFormatter object
+     * @return string  : HTML generated
+     */
+    public function renderAfterStep($obj);
+
+    /**
+     * To include CSS
+     * @return string  : HTML generated
+     */
+    public function getCSS();
+
+    /**
+     * To include JS
+     * @return string  : HTML generated
+     */
+    public function getJS();
+}
